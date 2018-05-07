11762d4b6eb8b84e08c2306840be558a03c89320
Mike <Mike@MIKE-PC>
2018-05-07 19:11:57 +0800

    Upload Source

---
+++ SimpleMath/Logic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMath
+{
+    public class Logic
+    {
+        public double Add(double number1, double number2)
+        {
+            return (number1 + number2);
+        }
+        public double Multiply(double number1, double number2)
+        {
+            return (number1 * number2);
+        }
+    }
+}
---
+++ Test/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleMath;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Test
+{
+    [TestClass]
+    public class Test
+    {
+        public Logic simpleMath;
+        public Test()
+        {
+            simpleMath = new Logic();
+        }
+
+        [TestMethod]
+        public void TestCase1()
+        {
+            double result = simpleMath.Add(1,1);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestCase2()
+        {
+            double result = simpleMath.Multiply(1, 1);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestCase3()
+        {
+            double result = simpleMath.Multiply(1, 1);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
